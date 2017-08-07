91462ef3d87f8591e8d977ae825964694796ac7c
bartekbart <bartlomiej@assembla.com>
2017-08-07 22:29:56 +0200

    adding empty components, emporting & importing SearchBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBRQ-QrwS4aGou3I8JbklRAqFeQPcwYZz0';
+
 // this component will produce some HTML output
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //display this component
