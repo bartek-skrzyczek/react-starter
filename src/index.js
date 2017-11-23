5a1209151854a53737fca2b5f1748b4503f19c36
bartekbart <bartlomiej@assembla.com>
2017-11-23 20:25:18 +0100

    implementing components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
-
 const API_KEY = 'AIzaSyBRQ-QrwS4aGou3I8JbklRAqFeQPcwYZz0';
 
 class App extends Component {
